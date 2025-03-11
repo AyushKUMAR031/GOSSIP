fffc799b2f826a7f039108c98991fe1913c4f915
AyushKUMAR031 <ayushkumar03192003@gmail.com>
2025-03-11 17:14:45 +0530

    a minor change

--- server/routes/messagesRoute.js
+++ server/routes/messagesRoute.js
@@ -1,7 +1,7 @@
 const { addMessage, getMessages } = require("../controllers/messagesController");
 const router = require("express").Router();
 
-router.post("/addmsg/", addMessage);
-router.post("/getmsg/", getMessages);
+router.post("/addmsg", addMessage);
+router.post("/getmsg", getMessages);
 
 module.exports = router;
